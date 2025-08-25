39a8499deed280debaa395c013726de5bcfd37f4
Florian Guggi <f.guggi@viewpointsystem.com>
2025-08-25 21:10:41 +0200

    feat: Remove _ab postfix from observation ids

--- playback/merge_excels.py
+++ playback/merge_excels.py
@@ -9,21 +9,29 @@
 import os
 import sys
 import pandas as pd
+from tkinter import filedialog
+
 
 def main():
     if len(sys.argv) < 3:
-        print("Usage: uv run merge_excels.py <input folder> <output.xlsx>")
-        sys.exit(1)
+        input_folder = filedialog.askdirectory(title="Select input folder")
+        output = filedialog.asksaveasfilename(title="Select output file", defaultextension=".xlsx")
+    else:
+        input_folder = sys.argv[1]
+        output = sys.argv[2]
 
-    source_folder = sys.argv[1]
-    files = map(lambda x: source_folder + x, os.listdir(source_folder))
+    files = map(lambda x: input_folder + x, os.listdir(input_folder))
     excels = map(pd.read_excel, files)
     with_id = map(create_unique_id, excels)
-    pd.concat(with_id).to_excel(sys.argv[2], index=False)
-    
+    pd.concat(with_id).to_excel(output, index=False)
+
+
 def create_unique_id(df: pd.DataFrame):
-    df["id"] = df["Observation id"].str.cat(df["Subject"], sep='_')
+    # there are some observations where 2 cameras were used. These have _a _b appended to their observation id
+    # which should be removed
+    df["id"] = df["Observation id"].str.rstrip("_ab").str.cat(df["Subject"], sep="_")
     return df
+
 
 if __name__ == "__main__":
     main()
