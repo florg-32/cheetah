bcca99f15843721777360efac56a615e534b8431
Florian Guggi <f.guggi@viewpointsystem.com>
2025-08-25 21:14:19 +0200

    chore: Add file selector to behavior count script

--- playback/count_behaviors.py
+++ playback/count_behaviors.py
@@ -8,10 +8,20 @@
 
 import sys
 import pandas as pd
+from tkinter import filedialog
+
 
 def main():
-    df = pd.read_excel(sys.argv[1])
-    df.pivot_table(index="id", columns="Behavior", aggfunc="size", fill_value=0).reset_index().to_excel(sys.argv[2])
+    if len(sys.argv) < 3:
+        input_file = filedialog.askopenfilename(title="Select input")
+        output_file = filedialog.asksaveasfile(title="Select output file")
+    else:
+        input_file = sys.argv[1]
+        output_file = sys.argv[2]
+
+    df = pd.read_excel(input_file)
+    df.pivot_table(index="id", columns="Behavior", aggfunc="size", fill_value=0).reset_index().to_excel(output_file)
+
 
 if __name__ == "__main__":
     main()
